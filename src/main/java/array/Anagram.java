c9a682adb0fc822b88ea9db1c9c993e67eca1f96
rob737 <robinsrivastava737@gmail.com>
2023-02-19 04:28:42 +0530

    rob737-algo : Anagram

---
+++ src/main/java/array/Anagram.java
@@ -0,0 +1,29 @@
+package array;
+
+import java.util.Arrays;
+import java.util.Scanner;
+
+public class Anagram {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        String strOne = sc.next();
+        String strTwo = sc.next();
+
+        System.out.println("Is Anagram : "+ isAnagram(strOne,strTwo));
+    }
+
+    private static boolean isAnagram(String strOne, String strTwo) {
+        int len = strOne.length();
+        if(strOne.length() != strTwo.length())
+            return false;
+        char[] sOne = strOne.toCharArray();
+        char[] sTwo = strTwo.toCharArray();
+        Arrays.sort(sOne);
+        Arrays.sort(sTwo);
+        for(int i = 0; i<len; i++){
+            if(sOne[i] != sTwo[i])
+                return false;
+        }
+        return true;
+    }
+}
