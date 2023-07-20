ffea6a96dbd39ba692f4f23552429ba9da2eada4
rob737 <robinsrivastava737@gmail.com>
2023-07-20 07:01:34 +0530

    rob737-algo : filter out Odd One

--- src/main/java/bitmap/FindTwoOdds.java
+++ src/main/java/bitmap/FindTwoOdds.java
@@ -7,9 +7,28 @@ package bitmap;
 * */
 public class FindTwoOdds {
     public static void main(String[] args) {
-        int[] arr = {4,3,4,4,4,5,5};
+        int[] arr = {1,3,2,3,3,1};
         int result = findXor(arr);
-        int brianKerninghamAlgorithm = result & ~(result - 1);
+        // This will give me number that has only rightmost bit set.
+        int rightMostSetBit = result & ~(result - 1);
+        findOddOccurring(arr,rightMostSetBit);
+    }
+
+    /*
+    * Logic : rightmost set bit in result implies that
+    * one number will have that bit as 1 and other number must have that bit as 0.
+    *
+    * */
+    private static void findOddOccurring(int[] arr, int rightMostSetBit) {
+        int first=0,second=0;
+        for(int val : arr){
+            // This will filter out number that has rightmost bit set as the result.
+            if((val & rightMostSetBit) == 1)
+                first=first^val;
+            else
+                second=second^val;
+        }
+        System.out.println("Two odd occurring numbers are : " + first + " , " + second);
     }
 
     private static int findXor(int[] arr) {
