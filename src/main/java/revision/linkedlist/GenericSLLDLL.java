6df96fdf9a28efb905b5ebbfc6635a31c3497e57
rob737 <robinsrivastava737@gmail.com>
2023-07-31 06:52:54 +0530

    rob737-algo : Generic Singly Linked List

---
+++ src/main/java/revision/linkedlist/GenericSLLDLL.java
@@ -0,0 +1,20 @@
+package revision.linkedlist;
+
+class Node<T> {
+    T data;
+    Node next;
+
+    Node(T val){
+        data = val;
+        next = null;
+    }
+}
+
+public class GenericSLLDLL {
+    public static void main(String[] args) {
+        Node one = new Node(5);
+        Node two = new Node("Generic");
+
+        System.out.println(one.data + " : " + two.data);
+    }
+}
