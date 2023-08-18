9170eb8399316f5914e77bb4079f3c69a7f77336
rob737 <robinsrivastava737@gmail.com>
2023-08-18 07:53:14 +0530

    rob737-algo : Binary Traversal

---
+++ src/main/java/revision/binarytree/BasicTraversal.java
@@ -0,0 +1,40 @@
+package revision.binarytree;
+
+public class BasicTraversal {
+
+    // Revise the use of static inner class.
+    static class Node {
+        int data;
+        Node left, right;
+
+        Node(int val){
+            data = val;
+            left=right=null;
+        }
+    }
+
+    public static void main(String[] args) {
+        Node root = createBST();
+    }
+
+    private static Node createBST() {
+        Node thirteen = new BasicTraversal.Node(13);
+        Node ten = new BasicTraversal.Node(10);
+        Node eight = new BasicTraversal.Node(8);
+        Node twelve = new BasicTraversal.Node(12);
+        Node eleven = new BasicTraversal.Node(11);
+        Node sixteen = new BasicTraversal.Node(16);
+        Node eighteen = new BasicTraversal.Node(18);
+        Node twenty = new BasicTraversal.Node(20);
+
+        thirteen.left=ten;
+        thirteen.right=eighteen;
+        ten.left=eight;
+        ten.right=twelve;
+        twelve.left=eleven;
+        twelve.right=sixteen;
+        eighteen.right=twenty;
+
+        return thirteen;
+    }
+}
