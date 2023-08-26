8366c5102a508cfba9285be9d9a95f981098d802
rob737 <robinsrivastava737@gmail.com>
2023-08-26 07:14:30 +0530

    rob737-algo : Power X By N

--- src/main/java/revision/backtracking/PowerXByN.java
+++ src/main/java/revision/backtracking/PowerXByN.java
@@ -1,4 +1,18 @@
 package revision.backtracking;
 
 public class PowerXByN {
+
+    public static void main(String[] args) {
+        int x=2, n = 10;
+        long result = computePower(x,n);
+        System.out.println("x ^ n = " + result);
+    }
+
+    // Time Complexity is O(n). Can we reduce it?
+    private static long computePower(int x, int n) {
+        if(n <= 1)
+            return x;
+        return x * computePower(x,n-1);
+    }
+
 }
