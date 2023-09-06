78135717d488fc0ad854cdc5d8d297ea6a4d376a
rob737 <robinsrivastava737@gmail.com>
2023-09-06 06:57:14 +0530

    rob737-algo : Power X By N in log N

--- src/main/java/revision/backtracking/PowerXByN.java
+++ src/main/java/revision/backtracking/PowerXByN.java
@@ -3,8 +3,9 @@ package revision.backtracking;
 public class PowerXByN {
 
     public static void main(String[] args) {
-        int x=2, n = 10;
-        long result = computePower(x,n);
+        int x=-2, n = 10;
+//        long result = computePower(x,n);
+        long result = computePowerOptimized(x,n);
         System.out.println("x ^ n = " + result);
     }
 
@@ -13,6 +14,18 @@ public class PowerXByN {
         if(n <= 1)
             return x;
         return x * computePower(x,n-1);
+    }
+
+    // Time Complexity is O(log N).
+    private static long computePowerOptimized(int x, int n){
+        if(n<=1)
+            return x;
+        long result = computePowerOptimized(x,n/2);
+        if(n % 2 == 0)
+            result = result * result;
+        else
+            result = x * result * result;
+        return result;
     }
 
 }
