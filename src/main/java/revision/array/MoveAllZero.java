248657316a807e33cbdb02799d32475a3c7b736b
rob737 <robinsrivastava737@gmail.com>
2023-09-08 06:48:32 +0530

    rob737-algo : Move All Zero to end

--- src/main/java/revision/array/MoveAllZero.java
+++ src/main/java/revision/array/MoveAllZero.java
@@ -1,7 +1,34 @@
 package revision.array;
 
+/*
+* Move all zeros to the end without sorting and maintaining relative position of other elements.
+* */
 public class MoveAllZero {
-    public static void main(String[] args) {
 
+    public static void main(String[] args) {
+        int [] input = {1,3,0,0,4,0,9};
+        moveZeroToEnd(input);
+        System.out.println("Result after modifying.");
+        System.out.println(input);
     }
+
+    private static void moveZeroToEnd(int[] input) {
+        /*
+        * I think, we can use 2 pointer technique here.
+        * first pointer will track index of element 0 and second will
+        * be used to swap non-zero element with 0.
+        * */
+        int front=0,rear=1;
+
+        while (front < rear){
+            if(front!=0)
+                front++;
+            if(front < rear && input[front] == 0 && input[rear]!=0){
+                int temp = input[front];
+                input[front] = input[rear];
+                input[rear] = temp;
+            }
+        }
+    }
+
 }
