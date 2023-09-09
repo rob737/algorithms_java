7cf2acc94cdd36df160ccd895abc1abe426ac0e4
rob737 <robinsrivastava737@gmail.com>
2023-09-09 08:11:33 +0530

    rob737-algo : Move All Zero to end

--- src/main/java/revision/array/MoveAllZero.java
+++ src/main/java/revision/array/MoveAllZero.java
@@ -1,5 +1,8 @@
 package revision.array;
 
+import java.util.ArrayList;
+import java.util.List;
+
 /*
 * Move all zeros to the end without sorting and maintaining relative position of other elements.
 * */
@@ -9,8 +12,14 @@ public class MoveAllZero {
         int [] input = {1,3,0,0,4,0,9};
         moveZeroToEnd(input);
         System.out.println("Result after modifying.");
-        System.out.println(input);
+        for(int val : input)
+        System.out.println(val);
     }
+
+
+
+
+
 
     private static void moveZeroToEnd(int[] input) {
         /*
@@ -19,15 +28,20 @@ public class MoveAllZero {
         * be used to swap non-zero element with 0.
         * */
         int front=0,rear=1;
+        int n = input.length;
 
-        while (front < rear){
-            if(front!=0)
+        while (rear < n){
+            if(input[front]!=0)
                 front++;
+
+
+
             if(front < rear && input[front] == 0 && input[rear]!=0){
                 int temp = input[front];
                 input[front] = input[rear];
                 input[rear] = temp;
             }
+            rear++;
         }
     }
 
