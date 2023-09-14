c8d3d28512dd1f2117965935b1fc85cee5bc44c5
rob737 <robinsrivastava737@gmail.com>
2023-09-14 07:11:21 +0530

    rob737-algo : Merge Sort

--- src/main/java/revision/array/MergeSort.java
+++ src/main/java/revision/array/MergeSort.java
@@ -1,4 +1,47 @@
 package revision.array;
 
 public class MergeSort {
+    private static int[] result;
+
+    public static void main(String[] args) {
+        int[] arr = {8,4,3,12,25,6,13,10};
+        int n = arr.length;
+        result = new int[n];
+        mergeSort(arr,0,n-1);
+        System.out.println("Merge Sorted Array");
+        for(int val : result)
+            System.out.print(val + " ");
+    }
+
+    private static void mergeSort(int[] arr, int begin, int end) {
+        int mid = (begin+end)/2;
+        if(begin >= mid)
+            return;
+
+        // No need to store return value as current value of begin, mid, end will correspond to atomic value.
+        mergeSort(arr,begin,mid);
+        mergeSort(arr,mid+1,end);
+
+        merge(arr,begin,mid,end);
+    }
+
+    private static void merge(int[] arr, int begin, int mid, int end) {
+        int k = begin, j = mid+1;
+
+        while (begin <= mid && j <= end){
+
+            if(arr[begin] <= arr[j]){
+                result[k++] = arr[begin++];
+            }else{
+                result[k++] = arr[j++];
+            }
+        }
+
+        while (begin<=mid)
+            result[k++]=arr[begin++];
+
+        while (j <= end)
+            result[k++]=arr[j++];
+
+    }
 }
