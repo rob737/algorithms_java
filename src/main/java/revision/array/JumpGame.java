1ed210a103826667651b0722f7fa2d03e7dafc69
rob737 <robinsrivastava737@gmail.com>
2023-10-01 17:32:06 +0530

    rob737-algo : Jump Game

---
+++ src/main/java/revision/array/JumpGame.java
@@ -0,0 +1,14 @@
+package revision.array;
+
+public class JumpGame {
+    public static void main(String[] args) {
+        int[] arr = {1,3,5,8,9,2,6,7,6,8,9};
+        int minimumJump = minJumpRequired(arr);
+        System.out.println("Minimum number os jumps required to reach last index : " + minimumJump);
+    }
+
+    private static int minJumpRequired(int[] arr) {
+
+        return -1;
+    }
+}
