d33391b26603092ed8dd43fe2c1b667374ee8c84
rob737 <robinsrivastava737@gmail.com>
2025-05-01 08:39:02 +0530

    FrequencyOfEachElement

---
+++ src/main/java/leetcode/revision/FrequencyOfEachElement.java
@@ -0,0 +1,22 @@
+package leetcode.revision;
+
+public class FrequencyOfEachElement {
+    public static void main(String[] args) {
+        int [] arr = {5,2,7,7,5,5,2};
+        findFrequencyOfEachElement(arr);
+    }
+
+    private static void findFrequencyOfEachElement(int[] arr) {
+        int n = arr.length, result=0;
+        for(int i =0; i<n; i++)
+            arr[i] = arr[i] - 1;
+        for(int i =0; i<n; i++){
+            arr[arr[i]%n] = arr[arr[i]%n]+n;
+        }
+
+        for(int i = 0; i<n; i++){
+            result = arr[i]/n;
+            System.out.println("Frquency of " + (i+1) + " : " + result);
+        }
+    }
+}
