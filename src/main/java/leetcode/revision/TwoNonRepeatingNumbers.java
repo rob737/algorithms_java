274c7d7992451703f6a2f990843d56bd6b677f0b
rob737 <robinsrivastava737@gmail.com>
2025-05-19 07:41:46 +0530

    TwoNonRepeatingNumbers

---
+++ src/main/java/leetcode/revision/TwoNonRepeatingNumbers.java
@@ -0,0 +1,19 @@
+package leetcode.revision;
+
+public class TwoNonRepeatingNumbers {
+    public static void main(String[] args) {
+        int[] arr = {2,4,7,9,2,4};
+        int result=0;
+        for(int val : arr)
+            result = result ^ val;
+        int rightMostBitSet = result & ~(result-1);
+        int a=0, b=0;
+        for(int val : arr){
+            if((val & rightMostBitSet) == 0)
+                a=a^val;
+            else
+                b=b^val;
+        }
+        System.out.println("Two non repeating number " + a + "," + b);
+    }
+}
